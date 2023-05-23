94b5c4e51c970f72f67486dc912c765e4c3f24c2
shihan79 <shihanaust@gmail.com>
2023-05-23 15:35:46 +0600

    comment added

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ let error2 = document.querySelector('.error2')
 let chance = document.querySelector('.chance')
 let player2 = document.querySelector('.player2')
 let count = 5
-
+// player1input
 player1button.addEventListener('click',function(){
     console.log(player1input.value)
+    // Null value reject
     if(!player1input.value){
         error1.innerHTML = 'please enter somthing'
     }else{
+        // NaN value reject
         if((NaN)){
         error1.innerHTML = 'please enter number'   
         }else{
+            // telling to put value between 1 t0 10
            if(player1input.value>0 && player1input.value<11){
               document.querySelector('.player1').style.display = 'none'
               player1input.style.display = 'none'
@@ -28,21 +31,27 @@ player1button.addEventListener('click',function(){
               chance.style.display = 'block'
               chance.innerHTML = `chance: ${count}`
               console.log('game start')
+            //   if input not between 1 t0 10
         }else{
             error1.innerHTML = 'please enter a number between 1 to 10'
         }
     }
     }
 })
+// player2input 
 player2button.addEventListener('click',function(){
+    // null value reject
     if(!player2input.value){
         error2.innerHTML = 'please enter something'
+
     }else{
+        // input within the limit
         if(player2input.value>0 && player2input.value<11){
 
     console.log(player2input.value)
     
     chance.innerHTML = `chance : ${count}`
+    // game won for player2
     if(player1input.value == player2input.value){
            player2.innerHTML = "player2 win"
            chance.style.display ='none'
@@ -50,23 +59,28 @@ player2button.addEventListener('click',function(){
               player2button.style.display = 'none'
               error2.style.display = 'none'
     }
-else{       
-    
+    // game won for player1
+else{      
         count = count-1
+        console.log(`chance:${count}`)
+        // no count left 
 if(count==0){
     player2.innerHTML = 'player1 win'
     chance.style.display ='none'
     player2input.style.display = 'none'
     player2button.style.display = 'none'
     error2.style.display = 'none'
+    //still  count left
 }else{
     chance.innerHTML = `chance : ${count}`
 }    
    
 }
+// if input not between 1 t0 10
     }else{
         error2.innerHTML = 'please enter a number between 1 to 10'
     }
 }
-})
+}
+)
 
